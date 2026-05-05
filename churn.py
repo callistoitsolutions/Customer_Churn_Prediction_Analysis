f0a81869e7fb065591196d4e08d6fb6b840f1f01
callistoitsolutions <124126575+callistoitsolutions@users.noreply.github.com>
2026-05-05 13:43:52 +0530

    Update churn.py

--- churn.py
+++ churn.py
@@ -2,7 +2,7 @@
 Sample Model Training Script
 Create a churn prediction model for the dashboard
 """
-
+import os
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
